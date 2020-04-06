7247129f29bf615325c36dc05c6e6f83e8265e34
parvizrovshanaliyev <parvizra@code.edu.az>
2020-04-06 13:14:29 +0400

    abstract factory start

---
+++ CSharp.DesignPatterns/CSharp.DesignPatterns.AbstractFactory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp.DesignPatterns.AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
