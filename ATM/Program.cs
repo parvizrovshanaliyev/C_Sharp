ab946d7bec69f3a3a1d3d92cac91bc036134f836
parvizrovshanaliyev <parvizra@code.edu.az>
2020-04-20 22:38:46 +0400

    two math problem solved

---
+++ ATM/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] banknote = { 200, 100, 50, 20, 10, 5, 1 };
+
+            int money, banknoteCount, i;
+
+            while (true)
+            {
+                Console.Write("Enter amount to pay : ");
+
+                money = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("==========================");
+
+                for (i = 0; i <= banknote.Length - 1; i++)
+                {
+                    banknoteCount = money / banknote[i];
+                    if (banknoteCount != 0)
+                        Console.WriteLine(banknoteCount.ToString() + " number : " + Convert.ToString(banknote[i] + " AZN "));
+                    money %= banknote[i];
+                }
+            }
+        }
+    }
+}
--- PrimeNumber/Program.cs
+++ PrimeNumber/Program.cs
@@ -10,6 +10,33 @@ namespace PrimeNumber
     {
         static void Main(string[] args)
         {
+
+
+            while (true)
+            {
+                Console.WriteLine("***************");
+                Console.WriteLine("Enter a number :");
+
+                int number = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+                bool isPrime = IsPrime(number);
+
+                Console.WriteLine(isPrime ? "number is prime" : "number is not prime");
+            }
+        }
+
+
+        static bool IsPrime(int n)
+        {
+            if(n <=1)
+                return false;
+
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+
+            return true;
         }
     }
 }
