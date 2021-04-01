357ed36ab7c8919bc348d9af8e0343a4b6894715
parvizrovshanaliyev <parvizra@code.edu.az>
2021-04-01 13:18:38 +0400

    Array

---
+++ Arrays/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+#region arrays
+/*
+ * bir deyisen altinda birden cox eyni tipli deyeri saxlamagimiza
+ *
+ * imkan verir.
+ *
+ * Demeli arrayler prosedual programlasdirmanin temel anlayislarindan
+ * olmaqla yanasi daha sonra oyreneceyimiz Collection-larinda temelini ozeyini
+ * teskil etmektedirler.
+ *
+ * Reference tipindedirler. Ozlerinde onlarda bir classdir deye bilerik.
+ *
+ * Demeli bunlar reference type olduqlarindan ramde -heap hissesinde saxlanirlir.
+ *
+ * Hemcinin Arrayler icerisindeki elemetleri daginiq sekilde deyil indexleyerek,
+ *
+ * sirali bir sekilde depolayir. Index her zaman 0-dan n-1 qeder gedir.
+ *
+ * [] - indexer adlanir.
+ *
+ * arrayler serhedi olan anlayislardir. Yeni nece element teyin edilecek sayi bildirilmelidir.
+ *
+ * sayi bildirilen arrayler daxil edilen say qeder ramde yer tuturlar (istifade etsekde etmesekde) bu zaman
+ *
+ * once tipin default deyeri sonra index nomresi qeyd olunur index : value
+ *
+ * qeyd edilen arrayin serhedini kecsek yeni int [7] 7 elementli bir arraye ondan elave
+ * element artira bilmerik. xeta verir
+ *
+ *
+ *
+ */
+//// array yaradilmasi
+//int[] ages = new int[10];
+
+//// arrayin elementlerine deyerlerin menimsedilmesi
+
+//ages[0] = 15;
+//ages[4] = 20;
+
+#region arraylerde loop
+
+//string[] products = new string[10];
+
+//for (int i = 0; i < products.Length; i++)
+//{
+//    products[i] = "product" + i;
+//    Console.WriteLine(products[i]);
+//}
+
+//Console.WriteLine(products.Length);
+
+#endregion
+
+#endregion
